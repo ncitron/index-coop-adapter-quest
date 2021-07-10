2b12e70e719336084afbc77d861bb1defc2345d8
JD <jann.driessen@gmx.de>
2021-07-10 17:47:11 +0200

    Add get trade call data

--- contracts/protocol/integration/exchange/UniswapV2ExchangeAdapter.sol
+++ contracts/protocol/integration/exchange/UniswapV2ExchangeAdapter.sol
@@ -41,74 +41,56 @@ contract UniswapV2ExchangeAdapter is IExchangeAdapter {
     }
 
     /* ============ External Getter Functions ============ */
-  
-      /**
+
+    /**
      * Calculate UniSwap trade encoded calldata. To be invoked on the SetToken.
      *
      * @param  _sourceToken              Address of source token to be sold
      * @param  _destinationToken         Address of destination token to buy
      * @param  _destinationAddress       Address to receive traded tokens
      * @param  _sourceQuantity           Amount of source token to sell
-     * @param _minDestinationQuantity    Minimum amount destinaton token to be recieved for the transacction not to revert
+     * @param _minDestinationQuantity    Min amount destinaton token to be recieved for the transacction not to revert
      * @param _data                      Call data
      *
      * @return address                   Target address
      * @return uint256                   Call value
      * @return bytes                     Trade calldata
      */
+    function getTradeCalldata(
+        address _sourceToken,
+        address _destinationToken,
+        address _destinationAddress,
+        uint256 _sourceQuantity,
+        uint256 _minDestinationQuantity,
+        bytes calldata _data
+    )
+        external
+        view
+        override
+        returns (address, uint256, bytes memory)
+    {
+        address[] memory path;
 
-
-     /* 
-     * Write getTradeCalldata function with the parameters / return values listed above. 
-     * The function will return 3 values: address of the uniswap router, 0 for Call value, trade calldata
-     */
-
-    function getTradeCalldata(/*YOUR CODE HERE*/) external view returns (/*YOUR CODE HERE*/){
-
-
-    /* 
-    * We have created a path for the address' of _sourceToken and _destinationToken 
-    * to be used in the function call to the uniswap contract.
-    */
-
-         address[] memory path;
-
-        if (_data.length == 0){
+        if (_data.length == 0) {
             path = new address[](2);
             path[0] = _sourceToken;
             path[1] = _destinationToken;
-        }else {
+        } else {
             path = abi.decode(_data, (address[]));
         }
-        
 
-    /* 
-    * Create a bytes memory variable called 'callData' to store the abi.encodedWithSignature data from Uniswap function swapExactTokensForTokens.  
-    * Please see README.md resources for more details on the Uniswap function swapExactTokensForTokens.
-    */   
+        // Encode method data for SetToken to invoke
+        bytes memory callData = abi.encodeWithSignature(
+            "swapExactTokensForTokens(uint256,uint256,address[],address,uint256)",
+            _sourceQuantity,
+            _minDestinationQuantity,
+            path,
+            _destinationAddress,
+            block.timestamp
+        );
 
-                    // YOUR CODE HERE
-
-
-          return (/*YOUR CODE HERE*/);
-
-     }
-
-
-
-/*
-* Write the getSpender() function that will return the address of our set Uniswap router. 
-* make sure the function is external view
-*/
-
-/** 
-*
-* Returns the UniSwap contract address.
-* @return address
-*
-*/
-
-                // YOUR CODE HERE
+        return (uniswapV2RouterAddress, 0, callData);
+    }
 
     /**
     *
