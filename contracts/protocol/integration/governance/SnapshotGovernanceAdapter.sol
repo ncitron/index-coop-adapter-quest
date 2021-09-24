acfe50d1830e22a242b19603efadfcc08b01f3b7
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-09-24 19:56:27 +0200

    getDelegateCalldata implementation + test

--- contracts/protocol/integration/governance/SnapshotGovernanceAdapter.sol
+++ contracts/protocol/integration/governance/SnapshotGovernanceAdapter.sol
@@ -9,12 +9,29 @@ pragma experimental "ABIEncoderV2";
  * Governance adapter for Snapshot
  */
 contract SnapshotGovernanceAdapter {
-    address public delegatee;
+    address public delegateRegistry;
+
+    // Signature of delegate function
+    string public constant DELEGATE_SIGNATURE = "setDelegate(bytes32,address)";
 
     /* ============ Constructor ============ */
 
-    constructor(address _delegatee) public {
-        delegatee = _delegatee;
+    constructor(address _delegateRegistry) public {
+        delegateRegistry = _delegateRegistry;
+    }
+
+    /**
+     * Generates the calldata to delegate votes to another ETH address. Self and zero address allowed, which is equivalent to registering and revoking in Aave.
+     *
+     * @param _delegatee            Address of the delegatee
+     *
+     * @return address              Target contract address
+     * @return uint256              Total quantity of ETH (Set to 0)
+     * @return bytes                setDelegate calldata
+     */
+    function getDelegateCalldata(address _delegatee) external view returns (address, uint256, bytes memory) {
+        bytes memory callData = abi.encodeWithSignature(DELEGATE_SIGNATURE, bytes32(0), _delegatee);
+        return (delegateRegistry, 0, callData);
     }
 }
 
--- test/protocol/integration/governance/snapshotGovernanceAdapter.spec.ts
+++ test/protocol/integration/governance/snapshotGovernanceAdapter.spec.ts
@@ -1,44 +1,71 @@
 import "module-alias/register";
+import { Address } from "@utils/types";
 import { Account } from "@utils/test/types";
 import DeployHelper from "@utils/deploys";
+import { ZERO_BYTES, ZERO } from "@utils/constants";
+import { SnapshotGovernanceAdapter, DelegateRegistry } from "@utils/contracts";
 import {
   addSnapshotBeforeRestoreAfterEach,
   getAccounts,
   getWaffleExpect,
+  getRandomAddress,
 } from "@utils/test/index";
-
 
 const expect = getWaffleExpect();
 
 describe("SnapshotGovernanceAdapter", () => {
   let owner: Account;
   let deployer: DeployHelper;
+  let snapshotGovernanceAdapter: SnapshotGovernanceAdapter;
+  let delegateRegistry: DelegateRegistry;
 
   before(async () => {
-    [
-      owner,
-    ] = await getAccounts();
+    [owner] = await getAccounts();
 
     deployer = new DeployHelper(owner.wallet);
+    delegateRegistry = await deployer.external.deployDelegateRegistry();
+    snapshotGovernanceAdapter = await deployer.adapters.deploySnapshotGovernanceAdapter(
+      delegateRegistry.address,
+    );
   });
 
   addSnapshotBeforeRestoreAfterEach();
 
   describe("#constructor", async () => {
-
     async function subject(): Promise<any> {
-      return deployer.adapters.deploySnapshotGovernanceAdapter(
-        owner.address,
-      );
+      return deployer.adapters.deploySnapshotGovernanceAdapter(delegateRegistry.address);
     }
 
-    it("should have the correct delegatee", async () => {
+    it("should have the correct delegate registry address", async () => {
       const deployedSnapashotGovernanceAdapter = await subject();
 
-      const actualDelegatee = await deployedSnapashotGovernanceAdapter.delegatee();
-      expect(actualDelegatee).to.eq(owner.address);
+      const actualTargetAddress = await deployedSnapashotGovernanceAdapter.delegateRegistry();
+      expect(actualTargetAddress).to.eq(delegateRegistry.address);
     });
-
   });
 
+  describe("#getDelegateCalldata", async () => {
+    let subjectDelegatee: Address;
+
+    beforeEach(async () => {
+      subjectDelegatee = await getRandomAddress();
+    });
+
+    async function subject(): Promise<any> {
+      return snapshotGovernanceAdapter.getDelegateCalldata(subjectDelegatee);
+    }
+
+    it("should return correct data for delegation", async () => {
+      const [targetAddress, ethValue, callData] = await subject();
+
+      const expectedCallData = delegateRegistry.interface.encodeFunctionData("setDelegate", [
+        ZERO_BYTES,
+        subjectDelegatee,
+      ]);
+
+      expect(targetAddress).to.eq(delegateRegistry.address);
+      expect(ethValue).to.eq(ZERO);
+      expect(callData).to.eq(expectedCallData);
+    });
+  });
 });
